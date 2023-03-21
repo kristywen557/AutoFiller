f5c2b440ca896d6e18de81a485193f61a58b4203
Enes557 <58561688+kristywen557@users.noreply.github.com>
2023-03-21 16:20:51 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
 const filler = require('@Enes557/AutoFiller');
-let words = filler.words(['stress','stream','steam','graphics','music','streoid'])
+let words = filler.words(['stress','stream','steam','graphics','music'])
 let string = 'stre'
-console.log(words.check(string)) // Output: [ 'stress', 'stream', 'streoid' ]
+console.log(words.check(string)) // Output: [ 'stress', 'stream' ]
