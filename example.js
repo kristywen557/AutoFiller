9ee6988bb2225ca0abd13882db4dfd327c0f8b17
Enes557 <58561688+kristywen557@users.noreply.github.com>
2023-03-22 23:09:54 +0300

    Add files via upload

--- example.js
+++ example.js
@@ -1,4 +1,13 @@
-const filler = require('@Enes557/AutoFiller');
-let words = filler.words(['stress','stream','steam','graphics','music'])
+const filler = require('autofiller');
+let words = filler.words(['stress','stream','steam','graphics','music','streoid'])
 let string = 'stre'
 console.log(words.check(string)) // Output: [ 'stress', 'stream' ]
+
+//Retrieve old data
+console.log(rwords.check(rtring))
+/*
+Output: {
+  result: [ 'stress', 'stream', 'streoid' ],
+  old: [ 'stress', 'stream', 'steam', 'graphics', 'music' ]
+}
+*/
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 module.exports = {
     words: (words) => {
         return {
-            check: (string) =>{
-                let result = [];
+            check: (string,invalid) =>{
+                let result = [],
+                old = []
                 for (i in words) {
                     if(words[i].includes(string)){
                         result.push(words[i]);
                     }
+                    if(invalid) old.push(words[i]);
                 }
+                if(invalid) return {
+                    result: result,
+                    old: old
+                };
                 return result;
             }
         }
