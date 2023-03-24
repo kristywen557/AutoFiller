c0d9ca4eb7c8384121c52bee14e574e289e2a4cf
Enes557 <58561688+kristywen557@users.noreply.github.com>
2023-03-24 23:57:26 +0300

    Add files via upload

--- example.js
+++ example.js
@@ -1,13 +1,16 @@
 const filler = require('./index');
 let words = filler.words(['stress','stream','steam','graphics','music'])
-let string = 'stre'
-console.log(words.check(string)) // Output: [ 'stress', 'stream' ]
+let string = 'str'
+//Adding with string variable
+words.add('strings')
+//Outputs
+console.log(words.check(string)) // Output: [ 'stress', 'stream', 'strings' ]
 
 //Retrieve old data
 console.log(words.check(string,true))
 /*
 Output: {
-  result: [ 'stress', 'stream' ],
+  result: [ 'stress', 'stream', 'strings' ],
   old: [ 'steam', 'graphics', 'music' ]
 }
 */
